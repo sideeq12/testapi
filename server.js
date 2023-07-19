016613e50e00b40d392b184de47805b2b162a29a
sideeq12 <abdwaheed2018@gmail.com>
2023-07-19 22:10:03 +0100

    setting api route

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res)=>{
+    res.json({
+        msg : "all list"
+    })
+})
+
+app.get("/students", (req, res)=>{
+    res.json({
+        msg : "student list"
+    })
+})
+
+
 
 app.listen(3000, ()=>{
     console.log("server running on port : 3000")
